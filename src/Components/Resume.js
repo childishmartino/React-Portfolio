55e880340af12d26242d29a01071bc41a5fce09c
Chris <chrisg1821@gmail.com>
2021-09-14 10:25:38 -0700

    updating bio line

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faFilePdf} from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import resume from '../assets/ChrisGarcia-Resume.pdf';
 
 function Resume() {
     return (
-        <Link to= {{ pathname: "/assets/ChrisGarcia-Resume.pdf"}} target="_blank" className="icon-link" download>
+        <Link to={resume} target="_blank" className="icon-link" download>
         <FontAwesomeIcon icon={faFilePdf} className="resume icon" />
         </Link>
     )
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -14,7 +14,7 @@ function Homepage() {
                     <span> Chris. </span>
                 </h1>
                 <p className="h-sub-text">
-                    I am an inquisitive, self-motivated and performance focused professional with a strong understanding of React, Javascript and project management. I just finished a Full Stack Development Bootcamp through the University of Denver. I am now looking to take the next step in my career and join an engineering team to apply my new development skills while continuing to learn.
+                    I am an inquisitive, self-motivated and performance focused professional with a strong understanding of React, MongoDB and project management. I just finished a Full Stack Development Bootcamp through the University of Denver. I am now looking to take the next step in my career and join an engineering team to apply my new development skills while continuing to learn.
                 </p>
                 <div className="icons">
                     <Resume />
